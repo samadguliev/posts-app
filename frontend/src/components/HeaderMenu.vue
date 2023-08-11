563ec6c6be5802ef90c6354b19cbba48d656ff18
Samad Guliev <samadguliev@mail.ru>
2023-08-11 14:13:53 +0300

    Styles

--- frontend/src/components/HeaderMenu.vue
+++ frontend/src/components/HeaderMenu.vue
@@ -1,12 +1,12 @@
 <template>
-  <div>
-    <ul>
-      <li>
+  <div class="header-menu-container">
+    <ul class="header-menu-list">
+      <li class="header-menu-element">
         <router-link to="/post-list">
           List
         </router-link>
       </li>
-      <li>
+      <li class="header-menu-element">
         <router-link to="/create-post">
           Create
         </router-link>
@@ -23,5 +23,27 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+.header-menu-container {
+  background-color: grey;
+  width: 100%;
+}
+
+.header-menu-list {
+  list-style: none;
+  display: flex;
+  justify-content: flex-start;
+  padding: 20px 40px;
+  font-size: 20px;
+  margin-top: 0;
+}
+
+.header-menu-element {
+  margin-right: 20px;
+}
+
+.header-menu-element a {
+  color: #fff;
+  text-decoration: none;
+}
 
 </style>
--- frontend/src/components/PostList.vue
+++ frontend/src/components/PostList.vue
@@ -1,6 +1,34 @@
 <template>
-  <div class="hello">
-    <h1>Post list</h1>
+  <div class="posts-container">
+    <h1 class="posts-header">Post list</h1>
+
+    <div class="post">
+      <p class="post-title">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit
+      </p>
+      <p class="post-description">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
+        dolore magna aliqua. Duis at tellus at urna condimentum. Ipsum dolor sit amet consectetur adipiscing elit.
+        Vitae nunc sed velit dignissim.
+      </p>
+      <p class="post-date">
+        22.07.2023
+      </p>
+    </div>
+
+    <div class="post">
+      <p class="post-title">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit
+      </p>
+      <p class="post-description">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
+        dolore magna aliqua. Duis at tellus at urna condimentum. Ipsum dolor sit amet consectetur adipiscing elit.
+        Vitae nunc sed velit dignissim.
+      </p>
+      <p class="post-date">
+        22.07.2023
+      </p>
+    </div>
   </div>
 </template>
 
@@ -12,5 +40,26 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+
+.posts-container {
+  margin-top: 50px;
+}
+
+.posts-header {
+  text-align: center;
+}
+
+.post {
+  max-width: 65%;
+  background-color: grey;
+  color: white;
+  padding: 30px;
+  border-radius: 10px;
+  margin: 0 auto 30px;
+}
+
+.post-title {
+  font-size: 18px;
+}
 
 </style>
