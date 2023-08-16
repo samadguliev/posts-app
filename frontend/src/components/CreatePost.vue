bf4bebb7db9daba6b0b7fc6542359bb5d0e89ad1
Samad Guliev <samadguliev@mail.ru>
2023-08-16 19:45:18 +0300

    Form styles

--- frontend/src/components/CreatePost.vue
+++ frontend/src/components/CreatePost.vue
@@ -1,16 +1,92 @@
 <template>
-  <div class="hello">
-    <h1>Create post</h1>
+  <div class="create-post-container">
+    <h1 class="create-post-header">
+      Create post
+    </h1>
+
+    <div class="create-post-form-container">
+      <input
+          class="create-post-input"
+          type="text"
+          v-model="title"
+          placeholder="Title"
+      >
+
+      <input
+          class="create-post-input"
+          type="text"
+          v-model="description"
+          placeholder="Description"
+      >
+
+      <textarea
+          class="create-post-text"
+          name="text"
+          cols="50"
+          rows="10"
+          v-model="text"
+      />
+    </div>
+
+    <button type="submit" class="create-post-button" @click="save()">
+      Save
+    </button>
   </div>
 </template>
 
 <script>
 export default {
   name: 'CreatePost',
+  data() {
+    return {
+      title: "",
+      description: "",
+      text: "",
+    }
+  },
+  methods: {
+    save() {
+      console.log('save')
+    }
+  }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+.create-post-header {
+  text-align: center;
+}
 
+.create-post-form-container {
+  max-width: 60%;
+  margin: 0 auto;
+}
+
+.create-post-input {
+  display: block;
+  min-width: 300px;
+  margin: 0 auto 30px;
+  padding: 10px 20px;
+  border-radius: 5px;
+  border-width: 1px;
+  font-size: 16px;
+}
+
+.create-post-text {
+  margin: 0 auto;
+  display: block;
+}
+
+.create-post-button {
+  display: block;
+  margin: 50px auto 0;
+  font-size: 16px;
+  padding: 10px;
+  width: 150px;
+  background-color: #00ce0d;
+  color: #fff;
+  border: 0;
+
+}
 </style>
